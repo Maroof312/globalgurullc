7bfa4a27f4bceef1044d426bfaa71167ee82de86
Maroof <maroof37993799@gmail.com>
2025-09-05 01:22:38 +0530

    Files

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  recaptchaSiteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY || "6Lca_r0rAAAAAIfMgP2C7SCNIHJ9QDO1BBqtrNrg",
+  recaptchaSiteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY || "6Le5Vb4rAAAAABYTZRZ86REphRfsLzXg1z1kiCUt",
   emailjsServiceId: import.meta.env.VITE_EMAILJS_SERVICE_ID || "service_ex2dlm8",
   emailjsTemplateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || "template_qeu3rti", 
   emailjsPublicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || "9MmQYqvuw8daVWWI2"
